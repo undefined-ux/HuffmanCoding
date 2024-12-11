8416234961b1851b34ba1d0ba24a7092af49d5ad
WartMxp <wartmxp@gmail.com>
2024-12-11 20:39:10 +0800

    Update Types.h

--- Process/Types.h
+++ Process/Types.h
@@ -4,13 +4,16 @@
 typedef char Datatype;
 typedef struct HTNode { //霍夫曼树节点
     int weight; //权
-    int parent; //父叶
+    int parent; //父节点
     int left_child, right_child; //左孩子，右孩子
-} *HuffmanTreeNode;
+} HuffmanTreeNode;
 typedef struct HTree {
     HuffmanTreeNode* data;
     int size;
 } HuffmanTree;
-
+typedef struct CLArguments {
+    FILE* input;
+    FILE* output;
+} CommandLineArgs;
 
 #endif
