83ba88351c39cc843175409c13e098a43c9b9c5c
WartMxp <wartmxp@gmail.com>
2024-12-11 17:54:51 +0800

    Add files via upload

---
+++ huffman.h
@@ -0,0 +1,11 @@
+//
+// Created by Lenovo on 2024/12/9.
+//
+
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+#include "Process/Encoder.h"
+#include "Process/Decoding.h"
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include "huffman.h"
+
+int main(int* argc, char** argv) {
+
+    //交互界面
+    int weight[256] = { 0 };
+
+
+
+
+    return 0;
+}
