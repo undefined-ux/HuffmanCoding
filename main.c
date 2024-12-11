988a8ca5a9f16359628172743082b92a2db0732d
WartMxp <wartmxp@gmail.com>
2024-12-11 20:48:35 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,21 @@
 #include <windows.h>
 #include "huffman.h"
 
-int main(int* argc, char** argv) {
+int weight[256] = { 0 };
 
-    //交互界面
-    int weight[256] = { 0 };
+CommandLineArgs parseCommandLineArgs(int argc, char* argv[]);
 
+int main(int argc, char* argv[]) {
 
+    const CommandLineArgs commandLineArgs = parseCommandLineArgs(argc, argv);
 
 
     return 0;
+}
+
+CommandLineArgs parseCommandLineArgs(int argc, char* argv[]) {
+    CommandLineArgs args;
+    args.input = stdin;
+    args.output = stdout;
+    return args;
 }
