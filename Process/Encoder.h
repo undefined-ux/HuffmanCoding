97925650a793963160fe81123bf2f3175cc255ac
WartMxp <wartmxp@gmail.com>
2024-12-11 20:34:23 +0800

    Update Encoder.h

--- Process/Encoder.h
+++ Process/Encoder.h
@@ -10,7 +10,7 @@ typedef struct HTNode {
     int weight;
     int parent;
     int left_child, right_child;
-} *HuffmanTreeNode;
+} HuffmanTreeNode;
 typedef struct HTree {
     HuffmanTreeNode* data;
     int size;
@@ -21,22 +21,23 @@ void Select(HuffmanTree* HT, int end, int* index1, int* index2) {
  	//选出两个权值最小的点
 	int min1 = INT_MAX, min2 = INT_MAX;
     for(int i = 0;i <= end;i ++) {
-    	if(HT->data[i].parent == -1 && HT -> data[i].weight < min1) {
+    	if(HT -> data[i].parent == -1 && HT -> data[i].weight < min1) {
         	min1 = HT -> data[i].weight;
             *index1 = i;
     	}
     }
     for(int i = 0;i <= end;i ++) {
-    	if(i !=  *index1 && HT.data[i].parent == -1 && HT -> data[i].weight < min2) {
+    	if(i !=  *index1 && HT -> data[i].parent == -1 && HT -> data[i].weight < min2) {
         	min2 = HT -> data[i].weight;
             *index2 = i;
     	}
     }
 }
+
 HuffmanTree* CreateHuffmanTree(int* weight, int n) {
   	//初始化
 	HuffmanTree* HT = (HuffmanTree*)malloc(sizeof(HuffmanTree));
-    HT -> data = (HuffmanTreeNode*)malloc(n * sizeof(HuffmanTreeNode*));
+    HT -> data = (HuffmanTreeNode*)malloc((2 * n - 1) * sizeof(HuffmanTreeNode*));
     HT -> size = 2 * n - 1;
     for(int i = 0;i < 2 * n - 1;i ++) {
       	if(i < n) {
@@ -46,11 +47,11 @@ HuffmanTree* CreateHuffmanTree(int* weight, int n) {
         HT -> data[i].left_child = -1;
         HT -> data[i].right_child = -1;
     }
-	//创建树
+	//生长
     for(int i = n;i < 2 * n - 1;i ++) {
 		int index1, index2;
         Select(HT, i - 1, &index1, &index2);
-        //删除
+        //重置父节点
         HT -> data[index1].parent = i;
         HT -> data[index2].parent = i;
         //合并
