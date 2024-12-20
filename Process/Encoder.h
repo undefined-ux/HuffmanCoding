7497580232880de6d9613f6fca934ad17d2b8fe5
WartMxp <wartmxp@gmail.com>
2024-12-20 20:28:20 +0800

    Update Encoder.h

--- Process/Encoder.h
+++ Process/Encoder.h
@@ -1,9 +1,7 @@
 #pragma once
 #include "Types.h"
 
-int WeightCmp(const CharWeight* f1, const CharWeight* f2);
-char CreateHuffmanCode(HuffmanTree* HT, CharWeight* Freq);
-HuffmanTree* CreateHuffmanTree(CharWeight* charweight, int n);
 void Select(HuffmanTree* HT, int end, int* index1, int* index2);
-
-#endif
+int WeightCmp(const CharWeight* f1, const CharWeight* f2);
+void GenerateHuffmanCodes(HuffmanTree* tree, CharWeight* ChWeight, int freq);
+HuffmanTree* CreateHuffmanTree(int* weight, int n);
