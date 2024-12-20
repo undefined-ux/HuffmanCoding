150893ba1cf39defe89ee66efcb9542f4e94675b
WartMxp <wartmxp@gmail.com>
2024-12-20 20:28:59 +0800

    Update Encoder.c

--- Process/Encoder.c
+++ Process/Encoder.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 #include "Types.h"
 #include "Encoder.h"
 
@@ -21,12 +24,13 @@ void Select(HuffmanTree* HT, int end, int* index1, int* index2) {
 int WeightCmp(const CharWeight* a, const CharWeight* b) {
     const CharWeight* f1 = (const CharWeight*) a;
     const CharWeight* f2 = (const CharWeight*) b;
-    if (f1 -> weight < f2 -> weight) {
+    if (f1->weight < f2->weight) {
         return -1; // f1 小于 f2
-    } else if (f1 -> weight > f2 -> weight) {
+    } else if (f1->weight > f2->weight) {
         return 1;  // f1 大于 f2
     } else {
         return 0;  // 两者相等
+    }
 }
 /*
 
@@ -44,15 +48,15 @@ typedef struct HTree {
 
 */
 
-HuffmanTree* CreateHuffmanTree(CharWeight* charweight, int n) {
+HuffmanTree* CreateHuffmanTree(int* weight, int n) {
+
     //初始化
     HuffmanTree* HT = (HuffmanTree*)malloc(sizeof(HuffmanTree));
-    qsort(charweight -> weight, n, sizeof(CharWeight), WeightCmp);
     HT -> data = (HuffmanTreeNode*)malloc((2 * n - 1) * sizeof(HuffmanTreeNode*));
     HT -> size = 2 * n - 1;
     for(int i = 0;i < 2 * n - 1;i ++) {
         if(i < n) {
-            HT -> data[i].weight = Freq -> frequency[i];
+            HT -> data[i].weight = weight[i];
         }
         HT -> data[i].parent = -1;
         HT -> data[i].left_child = -1;
@@ -73,9 +77,32 @@ HuffmanTree* CreateHuffmanTree(CharWeight* charweight, int n) {
     return HT;
 }
 
-char CreateHuffmanCode(HuffmanTree* HT, int* weight) {
+void GenerateHuffmanCodes(HuffmanTree* tree, CharWeight* ChWeight, int freq) {
+    // 生成霍夫曼编码的函数实现
+    char buffer[256];
+    for (int i = 0; i < freq; i++) {
+        int current = i;
+        int parent = tree -> data[current].parent;
+        int index = 0;
+        while (parent != -1) {
+            if (tree -> data[parent].left_child == current) {
+                buffer[index++] = '0';
+            } else {
+                buffer[index++] = '1';
+            }
+            current = parent;
+            parent = tree -> data[current].parent;
+        }
+        buffer[index] = '\0';
 
-    for (int i = 0; i < HT -> size; i++) {
-        weight[HT -> data[i].character] = i;
+        // 反转编码
+        for (int j = 0; j < index / 2; j++) {
+            char temp = buffer[j];
+            buffer[j] = buffer[index - j - 1];
+            buffer[index - j - 1] = temp;
+        }
+        //copy进ChWeight里
+        ChWeight[i].code = (char*)malloc((index + 1) * sizeof(char));
+        strcpy(ChWeight[i].code, buffer);
     }
 }
