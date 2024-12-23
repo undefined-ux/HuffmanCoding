7078e3419bd0a2391d22e3a042d00dd17021dbe3
WartMxp <wartmxp@gmail.com>
2024-12-23 21:46:49 +0800

    Update Input.h

--- Process/Input.h
+++ Process/Input.h
@@ -1,9 +1,6 @@
 #pragma once
 #include <stdio.h>
-#include "Types.h"
 
-void addCharWeight(CharWeight *Weight, int *size, char ch);
-void setInputAndOutputStream(FILE* file);
-void setInputStream(FILE* file,char* filename);
-void addOrUpdateCharFreq(CharFrequency *freqArray, int *size, char ch);
-Paragraph FileWriter(FILE* file,char* filename, int* weight);
+int CharCmp(const void* a, const void* b);
+void setInputStream(FILE* stream);
+void stdInput();
