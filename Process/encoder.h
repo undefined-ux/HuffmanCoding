520b9ee7656c0965429b4e6ca650b5bcf89515ae
WartMxp <wartmxp@gmail.com>
2024-12-23 21:40:11 +0800

    Update and rename Encoder.h to encoder.h

--- Process/encoder.h
+++ Process/encoder.h
@@ -3,5 +3,6 @@
 
 void Select(HuffmanTree* HT, int end, int* index1, int* index2);
 int WeightCmp(const CharWeight* f1, const CharWeight* f2);
-void GenerateHuffmanCodes(HuffmanTree* tree, CharWeight* ChWeight, int freq);
 HuffmanTree* CreateHuffmanTree(int* weight, int n);
+void CreateHuffmanCodes(HuffmanTree* tree, CharWeight* ChWeight, int freq);
+void SaveHuffmanCodes(CharWeight* ChWeight, size_t size, Paragraph* paragraph);
