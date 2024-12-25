8faca9cd1cf86a1ca646082de134e5dd8257664a
WartMxp <wartmxp@gmail.com>
2024-12-25 23:25:00 +0800

    delete a useless function

--- Process/encoder.h
+++ Process/encoder.h
@@ -2,7 +2,6 @@
 #include "Types.h"
 
 void Select(HuffmanTree* HT, int end, int* index1, int* index2);
-int WeightCmp(const CharWeight* f1, const CharWeight* f2);
-HuffmanTree* CreateHuffmanTree(int* weight, int n);
-void CreateHuffmanCodes(HuffmanTree* tree, CharWeight* ChWeight, int freq);
-void SaveHuffmanCodes(CharWeight* ChWeight, size_t size, Paragraph* paragraph);
+HuffmanTree* CreateHuffmanTree(CharWeight* ChWeight, int n);
+void CreateHuffmanCodes(HuffmanTree* HT, CharWeight* ChWeight, int freq);
+void SaveHuffmanCodes(const CharWeight* ChWeight, size_t size, Paragraph* paragraph);
